fa1e6aab7d2e9aa9984ac9f53317aeae2d7904f7
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2021-01-25 22:37:36 +0200

    Firebase Config file

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBaCtAAx1lZWRW26DPhRSBlNBHu6swO1uE",
+    authDomain: "e-commerce-project-224e3.firebaseapp.com",
+    projectId: "e-commerce-project-224e3",
+    storageBucket: "e-commerce-project-224e3.appspot.com",
+    messagingSenderId: "705711673609",
+    appId: "1:705711673609:web:2a11bc118041b477b683d8",
+    measurementId: "G-LM1VMTGXWK"
+};
