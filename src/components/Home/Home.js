e8644154123c2ea858954dc9db79551ff01bdd94
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2021-01-26 00:49:53 +0200

    Added Home component (main structure)

---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import '../../public/css/Home.css';
+// import  { Search, ShoppingBasket } from '@material-ui/icons';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image img-fluid" alt=" " src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220.jpg" />
+            
+                <div className="home__row">
+
+                </div>
+                <div className="home__row">
+
+                </div>
+                <div className="home__row">
+
+                </div>
+            </div>  
+        </div>
+    )
+}
+
+export default Home;
