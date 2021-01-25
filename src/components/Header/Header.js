2986b77a83ab28fc6cdea9cf1a0432206a27ceda
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2021-01-26 00:10:25 +0200

    Header component part 2 (completed)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 import '../../public/css/Header.css';
+import  { Search, ShoppingBasket } from '@material-ui/icons';
+
 
 function Header() {
     return (
         <div className="header">
-            <img className="header__logo img-fluid" 
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"   
-            />
+            <img className="header__logo img-fluid" alt="Logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
 
             <div className="header__search">
-                <input type="text" className="header__searchInput" />   
+                <input type="text" className="header__searchInput" /> 
+                <Search className="header__searchIcon" />  
             </div>
 
             <div className="header__nav">
@@ -36,9 +37,15 @@ function Header() {
                         Prime
                     </span>
                 </div>
+                <div className="header__optionBasket">
+                    <ShoppingBasket />
+                    <span className="header__optionLineTwo header__optionBasketCount">
+                        0
+                    </span>
+                </div>
             </div>  
         </div>
     )
 }
 
-export default Header
+export default Header;
