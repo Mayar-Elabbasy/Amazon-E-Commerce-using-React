c97100814cdaae4c0d6ad292d036f3bcd5fe4c20
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2021-01-26 23:47:44 +0200

    Added Checkout component (main structure)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Checkout from './components/Checkout/Checkout';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Switch>
-          <Route path="/checkout">
-            <Header />
-            <h1>checkout</h1>
-          </Route>
-          <Route path="/">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
+        <Header />
+          <Switch>
+            <Route path="/checkout">
+              <Checkout />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
       </div>
     </Router>
   );
