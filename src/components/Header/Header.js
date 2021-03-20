570fd63eaeee1b163d4f76a05694a890409bd6e9
Mayar-Elabbasy <mayarelabbasy2016@gmail.com>
2021-03-20 20:56:35 +0200

    added link to orders page if user is logged in & redirect to login if user is logged out

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -40,14 +40,31 @@ function Header() {
                         </span>
                     </div>
                 </Link>
-                <div className="header__option">
-                    <span className="header__optionLineOne">
-                        Returns
-                    </span>
-                    <span className="header__optionLineTwo">
-                        & Orders
-                    </span>
-                </div>
+
+                {state.user ?
+                    <Link to="/orders">
+                        <div className="header__option">
+                            <span className="header__optionLineOne">
+                                Returns
+                            </span>
+                            <span className="header__optionLineTwo">
+                                & Orders
+                            </span>
+                        </div>
+                    </Link>
+                    :
+                    <Link to="/login">
+                        <div className="header__option">
+                            <span className="header__optionLineOne">
+                                Returns
+                            </span>
+                            <span className="header__optionLineTwo">
+                                & Orders
+                            </span>
+                        </div>
+                    </Link>
+                }
+
                 <div className="header__option">
                     <span className="header__optionLineOne">
                         Your
